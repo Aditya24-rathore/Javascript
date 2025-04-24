3dc12b37cb619a63578e0927c532adbe90eb10ca
Aditya24-rathore <ra4402538@email.com>
2025-04-24 08:20:27 -0700

    update

---
+++ 22april/app.js
@@ -0,0 +1,27 @@
+// let pi=Math.PI;
+// let r=prompt("enter radius");
+// let area=pi*r*r;
+// console.log("Area of circle"+"="+area);
+
+
+// let a=prompt("enter a number");
+// if(a%2==0){
+//     console.log(a+""+"is a even number");
+// }
+// else{
+//     console.log(a+" "+"is a odd number");
+// }
+
+
+let num=parseInt(prompt("enter a number"));
+if(num>=1){
+    console.log("num is positive");
+}
+if(num==0){
+    console.log("it is zero");
+}
+else{
+    console.log("negative number");
+}
+
+
