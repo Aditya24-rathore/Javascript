ca82b4bba4a827c22f50f762f9eaa338ad59c542
Adityaraj Rathore <ra4402538@gmail.com>
2025-05-08 05:32:42 -0700

    update

---
+++ 8may/app.js
@@ -0,0 +1,20 @@
+let heading=document.getElementById("demo")
+heading.innerText="HELLO JS"
+
+let parent=document.getElementById("parent")
+parent.innerHTML=`<p>HELLO FROM JS </p>`
+parent.innerText=new Date()
+
+let myimg=document.getElementById("myimg")
+myimg.src="img2.jpeg"
+
+// changing style or css
+parent.style.color="red"
+parent.style.fontSize="20px"
+myimg.style.height="500px"
+
+document.write("HELLO")
+document.write(`<h2> HELLO FROM JS </h2>`)
+
+
+
